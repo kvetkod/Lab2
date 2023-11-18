5dfbbab9061fd6dd8c514650e09a32f809e96576
kvetkod <katekvetko@gmail.com>
2023-11-18 20:07:10 +0300

    chnglb2

--- Laptop.h
+++ Laptop.h
@@ -5,4 +5,13 @@
 class Laptop : public Computer {
 public:
 	Laptop(string model_, string ip_, string modelofprocessor_, int cores_, int speed_, int cachememory_, int memory_, bool keyboard_, bool connect_) : Computer(model_, ip_, modelofprocessor_, cores_, speed_, cachememory_, memory_, keyboard_, connect_) {};
+	void OpenLaptop();
+	void CloseLaptop();
+	void TurnOn();
+	void TurnOff();
+	bool GetState();
+	bool GetCover();
+private:
+	bool cover = false;
+	bool state;
 };
